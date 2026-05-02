867c3dc1e1f62c85bbc3f7a7137aaff3fafbced5
R_ema1 <remasalsulami962@gmail.com>
2026-05-03 00:08:32 +0300

    camera setup

--- js/scan.js
+++ js/scan.js
@@ -1,11 +1,35 @@
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
+const cameraBox = document.querySelector(".camera");
 const ctx = canvas.getContext("2d");
 
 canvas.width = video.videoWidth;
 canvas.height = video.videoHeight;
-
 ctx.drawImage(video, 0, 0);
+
+
+async function openCamera() {
+  let stream;
+  try {
+    
+    stream = await navigator.mediaDevices.getUserMedia({
+      //on
+      video: true,
+      //off
+      audio: false
+    });
+
+    video.srcObject = stream;
+    //remove the hidden
+    cameraBox.style.display = "block";
+
+    console.log("Camera opened");
+  } catch (error) {
+    console.error("Camera error:", error);
+    alert("Camera could not be opened");
+  }
+}
+
 
 
 async function testOCR() {
